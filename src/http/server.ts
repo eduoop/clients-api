5b6fd98d8afbc8799652487bc3cc2d0b00b0658d
Eduardo <eduardo.moraes@clicksoft.com.br>
2024-06-06 01:50:44 -0300

    init

---
+++ src/http/server.ts
@@ -0,0 +1,10 @@
+import express from "express";
+// import { router } from "../routes";
+
+const app = express();
+
+app.use(express.json());
+// app.use(router);
+app.listen(process.env.PORT ? Number(process.env.PORT) : 3000, () =>
+  console.log("HTTP server running "),
+);
